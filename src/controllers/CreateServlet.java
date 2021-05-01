a573ce01ee70256fc253fe7793b0142d8da5ee47
ikki.nishihata <vo.yuzu@gmail.com>
2021-05-02 07:09:09 +0900

    Add CreateServlet.java

--- src/controllers/CreateServlet.java
+++ src/controllers/CreateServlet.java
@@ -74,15 +74,6 @@ public class CreateServlet extends HttpServlet {
                 // indexのページにリダイレクト
                 response.sendRedirect(request.getContextPath() + "/index");
             }
-
-            em.getTransaction().begin();
-            em.persist(m);
-            em.getTransaction().commit();
-            request.getSession().setAttribute("flush", "登録が完了しました。");
-            em.close();
-
-            response.sendRedirect(request.getContextPath() + "/index");
         }
     }
-
 }
